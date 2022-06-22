381aa762b2e49a009d7da251c66e8e3737633ff2
Fedor <sharapov@igm.spb.ru>
2022-06-22 06:34:31 +0300

    Исправлено несоответствие полового признака у имени и фамилии

--- src/WebClient/Infrastructure/Generator/Generator.cs
+++ src/WebClient/Infrastructure/Generator/Generator.cs
@@ -1,20 +1,22 @@
 ﻿using Bogus;
+using System;
 using WebClient.Models;
 
 namespace WebClient.Infrastructure.Generator
 {
     public class Generator : IGenerator
     {
+        private static Random _rand;
         Faker _faker;
-
         public Generator()
         {
+            _rand = new Random();
             _faker = new Faker("ru");
         }
-
         public CustomerVM NewCustomer()
         {
-            return new CustomerVM { Firstname = _faker.Name.FirstName(), Lastname = _faker.Name.LastName() };
+            var gender = (Bogus.DataSets.Name.Gender)_rand.Next(2);
+            return new CustomerVM { Firstname = _faker.Name.FirstName(gender), Lastname = _faker.Name.LastName(gender) };
         }
     }
 }
